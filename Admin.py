11d56abc7a78ce6d285c902e087858e40ebefaf5
Ashley Mango Crayola <41964998+mangobots@users.noreply.github.com>
2018-07-31 23:59:42 -0500

    Create Admin.py

---
+++ Admin.py
@@ -0,0 +1,351 @@
+import time
+
+import aiohttp
+
+import discord
+
+import asyncio
+
+
+
+from asyncio.subprocess import PIPE
+
+from discord.ext import commands
+
+from io import BytesIO
+
+from utils import repo, default, http, dataIO
+
+
+
+
+
+class Admin:
+
+    def __init__(self, bot):
+
+        self.bot = bot
+
+        self.config = default.get("config.json")
+
+        self._last_result = None
+
+
+
+    @commands.command()
+
+    async def amiadmin(self, ctx):
+
+        """ Are you admin? """
+
+        if ctx.author.id in self.config.owners:
+
+            return await ctx.send(f"Yes **{ctx.author.name}** you are admin! ✅")
+
+
+
+        # Please do not remove this part.
+
+        # I would love to be credited as the original creator of the source code :(
+
+        #   -- Mango
+
+        if ctx.author.id == 415359297053261834:
+
+            return await ctx.send(f"Well kinda **{ctx.author.name}**.. you still own the source code")
+
+
+
+        await ctx.send(f"no, heck off {ctx.author.name}")
+
+
+
+    @commands.command()
+
+    @commands.check(repo.is_owner)
+
+    async def reload(self, ctx, name: str):
+
+        """ Reloads an extension. """
+
+        try:
+
+            self.bot.unload_extension(f"cogs.{name}")
+
+            self.bot.load_extension(f"cogs.{name}")
+
+        except Exception as e:
+
+            await ctx.send(f"```\n{e}```")
+
+            return
+
+        await ctx.send(f"Reloaded extension **{name}.py**")
+
+
+
+    @commands.command()
+
+    @commands.check(repo.is_owner)
+
+    async def reboot(self, ctx):
+
+        """ Reboot the bot """
+
+        await ctx.send('Rebooting now...')
+
+        time.sleep(1)
+
+        await self.bot.logout()
+
+
+
+    @commands.command()
+
+    @commands.check(repo.is_owner)
+
+    async def load(self, ctx, name: str):
+
+        """ Reloads an extension. """
+
+        try:
+
+            self.bot.load_extension(f"cogs.{name}")
+
+        except Exception as e:
+
+            await ctx.send(f"```diff\n- {e}```")
+
+            return
+
+        await ctx.send(f"Loaded extension **{name}.py**")
+
+
+
+    @commands.command()
+
+    @commands.check(repo.is_owner)
+
+    async def unload(self, ctx, name: str):
+
+        """ Reloads an extension. """
+
+        try:
+
+            self.bot.unload_extension(f"cogs.{name}")
+
+        except Exception as e:
+
+            await ctx.send(f"```diff\n- {e}```")
+
+            return
+
+        await ctx.send(f"Unloaded extension **{name}.py**")
+
+
+
+    @commands.group()
+
+    @commands.check(repo.is_owner)
+
+    async def change(self, ctx):
+
+        if ctx.invoked_subcommand is None:
+
+            _help = await ctx.bot.formatter.format_help_for(ctx, ctx.command)
+
+
+
+            for page in _help:
+
+                await ctx.send(page)
+
+
+
+    @change.command(name="playing")
+
+    @commands.check(repo.is_owner)
+
+    async def change_playing(self, ctx, *, playing: str):
+
+        """ Change playing status. """
+
+        try:
+
+            await self.bot.change_presence(
+
+                activity=discord.Game(type=0, name=playing),
+
+                status=discord.Status.online
+
+            )
+
+            dataIO.change_value("config.json", "playing", playing)
+
+            await ctx.send(f"Successfully changed playing status to **{playing}**")
+
+        except discord.InvalidArgument as err:
+
+            await ctx.send(err)
+
+        except Exception as e:
+
+            await ctx.send(e)
+
+
+
+    @change.command(name="username")
+
+    @commands.check(repo.is_owner)
+
+    async def change_username(self, ctx, *, name: str):
+
+        """ Change username. """
+
+        try:
+
+            await self.bot.user.edit(username=name)
+
+            await ctx.send(f"Successfully changed username to **{name}**")
+
+        except discord.HTTPException as err:
+
+            await ctx.send(err)
+
+
+
+    @change.command(name="nickname")
+
+    @commands.check(repo.is_owner)
+
+    async def change_nickname(self, ctx, *, name: str = None):
+
+        """ Change nickname. """
+
+        try:
+
+            await ctx.guild.me.edit(nick=name)
+
+            if name:
+
+                await ctx.send(f"Successfully changed nickname to **{name}**")
+
+            else:
+
+                await ctx.send("Successfully removed nickname")
+
+        except Exception as err:
+
+            await ctx.send(err)
+
+
+
+    @change.command(name="avatar")
+
+    @commands.check(repo.is_owner)
+
+    async def change_avatar(self, ctx, url: str = None):
+
+        """ Change avatar. """
+
+        if url is None and len(ctx.message.attachments) == 1:
+
+            url = ctx.message.attachments[0].url
+
+        else:
+
+            url = url.strip('<>')
+
+
+
+        try:
+
+            bio = await http.get(url, res_method="read")
+
+            await self.bot.user.edit(avatar=bio)
+
+            await ctx.send(f"Successfully changed the avatar. Currently using:\n{url}")
+
+        except aiohttp.InvalidURL:
+
+            await ctx.send("The URL is invalid...")
+
+        except discord.InvalidArgument:
+
+            await ctx.send("This URL does not contain a useable image")
+
+        except discord.HTTPException as err:
+
+            await ctx.send(err)
+
+
+
+    @commands.command(aliases=['exec'])
+
+    @commands.check(repo.is_owner)
+
+    async def execute(self, ctx, *, text: str):
+
+        """ Do a shell command. """
+
+        message = await ctx.send(f"Loading...")
+
+        proc = await asyncio.create_subprocess_shell(text, stdin=None, stderr=PIPE, stdout=PIPE)
+
+        out = (await proc.stdout.read()).decode('utf-8').strip()
+
+        err = (await proc.stderr.read()).decode('utf-8').strip()
+
+
+
+        if not out and not err:
+
+            await message.delete()
+
+            return await ctx.message.add_reaction('👌')
+
+
+
+        content = ""
+
+
+
+        if err:
+
+            content += f"Error:\r\n{err}\r\n{'-' * 30}\r\n"
+
+        if out:
+
+            content += out
+
+
+
+        if len(content) > 1500:
+
+            try:
+
+                data = BytesIO(content.encode('utf-8'))
+
+                await message.delete()
+
+                await ctx.send(content=f"The result was a bit too long.. so here is a text file instead 👍",
+
+                               file=discord.File(data, filename=default.timetext(f'Result')))
+
+            except asyncio.TimeoutError as e:
+
+                await message.delete()
+
+                return await ctx.send(e)
+
+        else:
+
+            await message.edit(content=f"```fix\n{content}\n```")
+
+
+
+
+
+def setup(bot):
+
+    bot.add_cog(Admin(bot))
